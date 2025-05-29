8d31698db96a73a9eeefd60797bb61e483d213d8
ilyalycha <ilyalav2323@gmail.com>
2025-05-30 00:55:24 +0300

    Написание скрипта класса данных
    Создание скрипта класса данных, а также файла json, его заполнение примерными данными.

---
+++ laba_tru/laba_tru/Lavreshin/Models/HousingPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_tru.Lavreshin.Models
+{
+    class HousingPrice
+    {
+        public int Year { get; set; }
+        public double OneRoomPrice { get; set; }
+        public double TwoRoomPrice { get; set; }
+        public double ThreeRoomPrice { get; set; }
+    }
+}

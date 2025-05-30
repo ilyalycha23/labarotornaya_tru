4552070cbd95ce6f3ca4707622a0ca9d2d5d1411
rostislavguap <rostislavsambo@gmail.com>
2025-05-30 04:26:44 +0300

    Создание необходимых классов.
    Были созданы некоторые классы. Класс-модель,  который представляет структуру данных. Класс для загрузки данных из JSON-файла и их предварительной обработки. Класс для анализа данных, находит болезнь с максимальным и минимальным снижением заболеваемости.

---
+++ laba_tru/laba_tru/Pokalo/DataLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;  // для работы с File
+using System.Linq;
+using Newtonsoft.Json;  // Для работы с JSON
+
+namespace laba_tru.Pokalo
+{
+    public static class DataLoader
+    {
+        public static List<DiseaseData> LoadData(string filePath)
+        {
+            try
+            {
+                if (filePath.EndsWith(".json"))
+                {
+                    string json = File.ReadAllText(filePath);
+                    var data = JsonConvert.DeserializeObject<List<DiseaseData>>(json);
+                    CalculateYearlyChanges(data);
+                    return data;
+                }
+                throw new NotSupportedException("Поддерживаются только JSON файлы");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка загрузки данных: {ex.Message}");
+            }
+        }
+
+        private static void CalculateYearlyChanges(List<DiseaseData> data)
+        {
+            var diseases = data.Select(d => d.DiseaseName).Distinct();
+
+            foreach (var disease in diseases)
+            {
+                var yearlyData = data
+                    .Where(d => d.DiseaseName == disease)
+                    .OrderBy(d => d.Year)
+                    .ToList();
+
+                for (int i = 1; i < yearlyData.Count; i++)
+                {
+                    double change = (yearlyData[i].CasesCount - yearlyData[i - 1].CasesCount) /
+                                   (double)yearlyData[i - 1].CasesCount * 100;
+                    yearlyData[i].YearlyChangePercent = change;
+                }
+            }
+        }
+    }
+}
---
+++ laba_tru/laba_tru/Pokalo/DiseaseData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_tru.Pokalo
+{
+    public class DiseaseData
+    {
+        public int Year { get; set; }
+        public string DiseaseName { get; set; }
+        public int CasesCount { get; set; }
+
+        // Для анализа изменений
+        public double YearlyChangePercent { get; set; }
+    }
+}

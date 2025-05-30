bb1b78588a82749784897056c7b97832fc7d3949
rostislavguap <rostislavsambo@gmail.com>
2025-05-30 17:46:16 +0300

    Разработка функционала формы.
    Форма была связана с классами и разработана логика функций кнопок.

--- laba_tru/laba_tru/Pokalo/Form3.Designer.cs
+++ laba_tru/laba_tru/Pokalo/Form3.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
@@ -39,6 +39,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -48,30 +49,32 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(535, 46);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 200);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 67);
+            this.dataGridView1.Size = new System.Drawing.Size(190, 67);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // chart1
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
-            this.chart1.Location = new System.Drawing.Point(218, 239);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(218, 30);
             this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(570, 199);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(570, 408);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(651, 134);
+            this.button1.Location = new System.Drawing.Point(15, 289);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 23);
             this.button1.TabIndex = 2;
@@ -85,6 +88,7 @@
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(90, 20);
             this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDown2
             // 
@@ -118,12 +122,23 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Построить прогноз";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -152,5 +167,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- laba_tru/laba_tru/Pokalo/Form3.cs
+++ laba_tru/laba_tru/Pokalo/Form3.cs
@@ -7,24 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.DataVisualization;
 
 namespace laba_tru.Pokalo
 {
     public partial class Form3 : Form
     {
+        private List<DiseaseData> _diseaseData;
+        private Dictionary<string, Color> _diseaseColors;
+
         public Form3()
         {
             InitializeComponent();
+            InitializeChart();
+            InitializeDiseaseColors();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Titles.Add("Динамика инфекционных заболеваний в России");
+            chart1.ChartAreas[0].AxisX.Title = "Год";
+            chart1.ChartAreas[0].AxisY.Title = "Количество случаев";
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+
+            // Удаляем существующую легенду (если есть)
+            if (chart1.Legends.Count > 0)
+            {
+                chart1.Legends.Clear();
+            }
+
+            // Создаем новую легенду
+            Legend legend = new Legend();
+            legend.Name = "MainLegend";
+            legend.Docking = Docking.Bottom;
+            chart1.Legends.Add(legend);
+        }
+
+        private void InitializeDiseaseColors()
+        {
+            _diseaseColors = new Dictionary<string, Color>
+            {
+                { "Грипп", Color.Blue },
+                { "Корь", Color.Green },
+                { "Туберкулез", Color.Orange },
+                { "Ветряная оспа", Color.Purple },
+                { "Коклюш", Color.Brown }
+            };
+        }
+
+        private void DisplayDataInGrid()
+        {
+            dataGridView1.DataSource = _diseaseData
+                .OrderBy(d => d.DiseaseName)
+                .ThenBy(d => d.Year)
+                .ToList();
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void BuildInitialCharts()
+        {
+            chart1.Series.Clear();
+
+            foreach (var disease in _diseaseData.Select(d => d.DiseaseName).Distinct())
+            {
+                var series = new Series(disease)
+                {
+                    ChartType = SeriesChartType.Line,
+                    BorderWidth = 3,
+                    Color = _diseaseColors.ContainsKey(disease) ? _diseaseColors[disease] : Color.Gray
+                };
+
+                foreach (var point in _diseaseData.Where(d => d.DiseaseName == disease).OrderBy(d => d.Year))
+                {
+                    series.Points.AddXY(point.Year, point.CasesCount);
+                }
+
+                chart1.Series.Add(series);
+            }
+        }
+
+        private void AnalyzeData()
+        {
+            // Пример простого анализа - можно расширить
+            var maxCases = _diseaseData.OrderByDescending(d => d.CasesCount).First();
+            label1.Text = $"Макс. случаев: {maxCases.DiseaseName} ({maxCases.CasesCount} в {maxCases.Year} г.";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog
+                {
+                    Filter = "JSON files (*.json)|*.json",
+                    Title = "Выберите файл с данными"
+                };
 
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _diseaseData = DataLoader.LoadData(openFileDialog.FileName);
+                    DisplayDataInGrid();
+                    BuildInitialCharts();
+                    AnalyzeData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            try
+            {
+                int windowSize = (int)numericUpDown1.Value;
+                int forecastYears = (int)numericUpDown2.Value;
+                int lastYear = _diseaseData.Max(d => d.Year);
 
+                foreach (var disease in _diseaseData.Select(d => d.DiseaseName).Distinct())
+                {
+                    var diseaseData = _diseaseData
+                        .Where(d => d.DiseaseName == disease)
+                        .OrderBy(d => d.Year)
+                        .Select(d => (double)d.CasesCount)
+                        .ToList();
+
+                    var forecast = MovingAverageCalculator.Forecast(diseaseData, windowSize, forecastYears);
+                    var series = chart1.Series.FindByName(disease);
+
+                    for (int i = 0; i < forecast.Count; i++)
+                    {
+                        DataPoint point = new DataPoint(lastYear + i + 1, forecast[i])
+                        {
+                            Color = Color.Red,
+                            BorderWidth = 2,
+                            MarkerStyle = MarkerStyle.Circle,
+                            MarkerSize = 8
+                        };
+                        series.Points.Add(point);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка прогнозирования: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
+        // Остальные методы-заглушки
+        private void label1_Click(object sender, EventArgs e) { }
+        private void chart1_Click(object sender, EventArgs e) { }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) { }
     }
 }
